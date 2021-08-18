f5d9a9e3167bbc170ad054eeffb5594369d0b212
GabeMoraes <74752286+GabeMoraes@users.noreply.github.com>
2021-08-18 19:25:31 -0300

    Add files via upload

---
+++ js/chamado.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    
+    $('#adicionar').click(function (e) { 
+        $('#tabela_chamado').toggle();
+        $('#tela_chamado').toggle();
+    });
+
+});
